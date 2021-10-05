5f564e30610b81591b3982b242e62ccbb986f374
Whenrixon <24whenriksson@woodward.edu>
2021-10-05 16:15:50 -0400

    Arm with Encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Arm.java
@@ -6,41 +6,41 @@ public class Arm {
 
     public DcMotor arm;
 
-  /*  public Arm (DcMotor armMotor){
+   public Arm (DcMotor armMotor){
         this.arm = armMotor;
         this.arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
 
     public void ground(){
-        arm.setTargetPosition();//-550
-        arm.setPower();//-1
+        arm.setTargetPosition(-550);//-550
+        arm.setPower(-1);//-1
         this.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void driving{
-        arm.setTargetPosition();//-550
-        arm.setPower();//-1
+    public void driving(){
+        arm.setTargetPosition(-600);//-550
+        arm.setPower(-1);//-1
         this.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void low(){
-        arm.setTargetPosition();//-550
-        arm.setPower();//-1
+        arm.setTargetPosition(-650);//-550
+        arm.setPower(-1);//-1
         this.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void medium(){
-        arm.setTargetPosition();//-550
-        arm.setPower();//-1
+        arm.setTargetPosition(-700);//-550
+        arm.setPower(-1);//-1
         this.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void high(){
-        arm.setTargetPosition();//-550
-        arm.setPower();//-1
+        arm.setTargetPosition(-750);//-550
+        arm.setPower(-1);//-1
         this.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-    */
+    
     public void armOff(){
         arm.setPower(0);
     }
